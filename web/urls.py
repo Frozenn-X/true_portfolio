c2b3016c75c4dfd88b818e28d9208fe81cddd8c4
frozenn <trauchessec.xavier@outlook.com>
2023-04-13 13:40:56 +0200

    up to end this shit today

--- web/urls.py
+++ web/urls.py
@@ -16,8 +16,23 @@ urlpatterns = [
     # page contact
     path('contact/', views.contact, name='contact'),
     path('contact', views.contact, name='contact'),
-
+    # COMPETENCES
+        ### TECHNIQUES
     path('api-desc', views.apis, name='api-desc'),
     path('python-desc', views.python, name='python-desc'),
     path('sql-desc', views.sql, name='sql-desc'),
+    path('bdd-relationnal-desc', views.bdd_relationnel, name='bdd-relationnal-desc'),
+    path('r&d-desc', views.research_and_dev, name='r&d-desc'),
+        ### HUMAINES
+    path('project-desc', views.project_gestion, name='project-desc'),
+    path('team-desc', views.team_gestion, name='team-desc'),
+    path('english-desc', views.english, name='english-desc'),
+    path('comm-desc', views.communication, name='comm-desc'),
+    # PROJECTS
+    path('wurth-project', views.wurth, name='wurth-project'),
+    path('SAM_DLF-project', views.sam_and_DLF, name='SAM_DLF-project'),
+    path('datactive-project', views.datactive, name='datactive-project'),
+    path('scraping-project', views.scraping_web, name='scraping-project'),
+    path('generation_email-project', views.generation_email, name='generation_email-project'),
+    path('hierarchy-project', views.qualif_hierarchy, name='hierarchy-project'),
 ]
--- web/views.py
+++ web/views.py
@@ -22,27 +22,68 @@ def login(request):
                       context={'error_msg':'it Seems you connection as no available, if you think is an error, contact me at : x.trauchessec@groupe-aen.info'})
     login(user=user)
     return redirect(reverse('home'))
-
-
+##############################################################################################################################
+######################################################## PAGE GENERAL ########################################################
+##############################################################################################################################
 def home(request):
     context = {'menu_actif': 'home'}
     return render(request, 'web/home.html', context=context)
-
 
 def about(request):
     context = {'menu_actif': 'about'}
     return render(request, 'web/about.html', context=context)
 
-
 def contact(request):
     context = {'menu_actif': 'contact'}
     return render(request, 'web/contact.html', context=context)
-
+##############################################################################################################################
+#################################################### COMPETENCE TECHNIQUE ####################################################
+##############################################################################################################################
 def sql(request):
-    return render(request=request, template_name='web/competence/sql.html', context={})
+    return render(request=request, template_name='web/competence/technique/sql.html', context={})
 
 def apis(request):
-    return render(request=request, template_name='web/competence/api.html', context={})
+    return render(request=request, template_name='web/competence/technique/api.html', context={})
 
 def python(request):
-    return render(request=request, template_name='web/competence/python.html', context={})
+    return render(request=request, template_name='web/competence/technique/python.html', context={})
+
+def bdd_relationnel(request):
+    return render(request=request, template_name='web/competence/technique/bdd_relationnel.html', context={})
+
+def research_and_dev(request):
+    return render(request=request, template_name='web/competence/technique/R&D.html', context={})
+##############################################################################################################################
+##################################################### COMPETENCE HUMAINE #####################################################
+##############################################################################################################################
+def english(request):
+    return render(request=request, template_name='web/competence/humaine/english.html', context={})
+
+def communication(request):
+    return render(request=request, template_name='web/competence/humaine/communication.html', context={})
+
+def team_gestion(request):
+    return render(request=request, template_name='web/competence/humaine/team_gestion.html', context={})
+
+def project_gestion(request):
+    return render(request=request, template_name='web/competence/humaine/project_gestion.html', context={})
+##############################################################################################################################
+######################################################## PAGE PROJECT ########################################################
+##############################################################################################################################
+def qualif_hierarchy(request):
+    return render(request=request, template_name='web/my_projects/qualif_hierarchy.html', context={})
+
+def wurth(request):
+    return render(request=request, template_name='web/my_projects/wurth.html', context={})
+
+def scraping_web(request):
+    return render(request=request, template_name='web/my_projects/scraping_web.html', context={})
+
+def sam_and_DLF(request):
+    return render(request=request, template_name='web/my_projects/sam_and_DLF.html', context={})
+
+def datactive(request):
+    return render(request=request, template_name='web/my_projects/datactive.html', context={})
+
+def generation_email(request):
+    return render(request=request, template_name='web/my_projects/generation_email.html', context={})
